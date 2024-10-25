5fa4d40a9ac4baa977442756a31cacdc8a7799d7
ulkiller <ulkersirinov@gmail.com>
2024-10-25 12:17:51 +0400

    first

---
+++ src/main/java/com/midterm/axirinci/AxirinciApplication.java
@@ -0,0 +1,13 @@
+package com.midterm.axirinci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxirinciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxirinciApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/midterm/axirinci/AxirinciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midterm.axirinci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxirinciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
